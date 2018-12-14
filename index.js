fd140c209822b6228ee7189056e3dd735dbf4eaa
Dan Wood <wooddw6@mc-531269.immediate.media>
2018-12-14 15:10:51 +0000

    Allow themes to return a promise

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function styleLoader(source, map) {
+  const callback = this.async();
   const themeName = (this.query && this.query.theme) || '';
-  let theme;
 
+  const buildResult = (result, source) => `${result}
+${source}`;
+
+  let theme;
   try {
     theme = require(themeName);
   } catch (e) {
@@ -9,15 +13,23 @@ function styleLoader(source, map) {
     theme = () => '';
   }
 
-  if (typeof theme !== 'function' || typeof theme() !== 'string') {
-    process.emitWarning(`sass-varaibles-loader error in  ${themeName}: theme must return a string`);
-    theme = () => '';
+  const result = theme();
+  if (typeof result === 'string') {
+    callback(null, buildResult(result, source), map);
   }
 
-  const output = `${theme()}
-${source}`;
-
-  this.callback(null, output, map);
+  if (Promise.resolve(result) === result) {
+    result
+      .then(output => {
+        callback(null, buildResult(output, source), map);
+      })
+      .catch(error => {
+        process.emitWarning(
+          `sass-varaibles-loader error in ${themeName}: theme must emit a string`,
+        );
+        callback(null, source, map);
+      });
+  }
 }
 
 module.exports = styleLoader;
